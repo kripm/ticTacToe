79bbce1dd419e7a3cdfa0f3240eb82a2f8f76271
kri <93227689+kripm@users.noreply.github.com>
2021-11-06 04:30:40 +0400

    Added default case.

--- src/myself/GameState.java
+++ src/myself/GameState.java
@@ -1,22 +1,21 @@
 package myself;
 
 public enum GameState {
-    UNFINISHED,
-    DRAW,
-    O_WIN,
-    X_WIN;
+    UNFINISHED, DRAW, O_WIN, X_WIN;
 
-    void check (GameState gameState) {
+    void check(GameState gameState) {
         switch (gameState) {
-            case X_WIN:
-                System.out.println("X wins");
-                break;
-            case O_WIN:
-                System.out.println("O wins");
-                break;
-            case DRAW:
-                System.out.println("Draw");
-                break;
+        case X_WIN:
+            System.out.println("X wins");
+            break;
+        case O_WIN:
+            System.out.println("O wins");
+            break;
+        case DRAW:
+            System.out.println("Draw");
+            break;
+        default:
+            break;
         }
     }
 }
