ab6609f51b0a52dd94b99641d4e76421ba23c56b
kri <93227689+kripm@users.noreply.github.com>
2021-11-11 12:39:52 +0400

    Update MenuPanel.java

--- src/myself/gui/MenuPanel.java
+++ src/myself/gui/MenuPanel.java
@@ -12,12 +12,12 @@ import java.awt.Font;
 public class MenuPanel extends JPanel {
 
     MenuPanel() {
-        ImageIcon imageX = new ImageIcon("src/myself/gui/Images/X.png");
-        Image image = imageX.getImage(); 
-        Image newImg = image.getScaledInstance(120, 120,  java.awt.Image.SCALE_SMOOTH); 
+        ImageIcon imageX = new ImageIcon("src/myself/gui/images/X.png");
+        Image image = imageX.getImage();
+        Image newImg = image.getScaledInstance(120, 120, java.awt.Image.SCALE_SMOOTH);
         ImageIcon smallX = new ImageIcon(newImg); // i stole this from Stack Overflow.
 
-        ImageIcon imageO = new ImageIcon("src/myself/gui/Images/O.png");
+        ImageIcon imageO = new ImageIcon("src/myself/gui/images/O.png");
         Image image2 = imageO.getImage();
         Image newImg2 = image2.getScaledInstance(120, 120, java.awt.Image.SCALE_SMOOTH);
         ImageIcon smallO = new ImageIcon(newImg2);
@@ -26,7 +26,6 @@ public class MenuPanel extends JPanel {
         Font f1 = new Font(Font.SANS_SERIF, Font.PLAIN, 25);
         JButton xButton = new JButton(smallX);
         JButton oButton = new JButton(smallO);
-        
 
         text.setLayout(null);
         text.setFont(f1);
@@ -35,13 +34,13 @@ public class MenuPanel extends JPanel {
         xButton.setContentAreaFilled(false);
         xButton.setLayout(null);
         xButton.setBounds(120, 220, 120, 120);
-        xButton.setCursor(new Cursor (Cursor.HAND_CURSOR));
+        xButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
         oButton.setContentAreaFilled(false);
         oButton.setLayout(null);
         oButton.setBounds(350, 220, 120, 120);
-        oButton.setCursor(new Cursor (Cursor.HAND_CURSOR));
-        
+        oButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
         this.setLayout(null);
         this.setBounds(0, 0, 600, 600);
         this.setLocation(0, 0);
