b1b5b3bfb1f4dbcc50c5a4c339827132e01355a5
kri <93227689+kripm@users.noreply.github.com>
2021-11-11 12:40:50 +0400

    Another work in progress...

---
+++ src/myself/gui/MenuBotPanel.java
@@ -0,0 +1,12 @@
+package myself.gui;
+
+import javax.swing.JPanel;
+
+public class MenuBotPanel extends JPanel {
+
+    MenuBotPanel() {
+        this.setLayout(null);
+        this.setBounds(0, 0, 600, 600);
+        this.setLocation(0, 0);
+    }
+}
