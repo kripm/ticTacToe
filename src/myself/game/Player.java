886e44423e1b775e704920d560ef0a87c49b6d4c
kri <93227689+kripm@users.noreply.github.com>
2021-11-11 12:36:54 +0400

    Update Player.java

--- src/myself/game/Player.java
+++ src/myself/game/Player.java
@@ -1,38 +1,21 @@
 package myself.game;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class Player {
     private char mark;
-    static Scanner sc = new Scanner(System.in);
 
-    void move() {
-        boolean validInput = false;
-        while (!validInput) {
-            try {
-                System.out.println("Enter the square number: ");
-                int square = sc.nextInt();
-                if (!Board.checkPossibleMove(square)) {
-                    System.out.println("This cell is occupied! Choose another one!");
-                } else if (square < 0 || square > 8) {
-                    throw new IndexOutOfBoundsException();
-                } else {
-                    Board.place(square, mark);
-                    validInput = true;
-                }
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println("Coordinates should be from 0 to 8!");
-            } catch (InputMismatchException e) {
-                System.out.println("You should enter numbers!");
-                sc.next(); // thank you stack overflow.
-            } catch (Exception e) {
-                System.out.println("Unknown error.");
-            }
+    public boolean move(int tile) {
+        if (Board.getMarkAt(tile - 1) == ' ') {
+            Board.place(tile - 1, mark);
+            return true;
         }
+        return false;
     }
 
-    void isX(boolean answer) {
+    public char getMark() {
+        return mark;
+    }
+
+    public void isX(boolean answer) {
         if (answer) {
             this.mark = 'X';
         } else {
