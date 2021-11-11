14d1593f1056bae508a7fef6c1a590f911ab8259
kri <93227689+kripm@users.noreply.github.com>
2021-11-11 12:38:40 +0400

    Added a "Play" button.

--- src/myself/gui/CLayout.java
+++ src/myself/gui/CLayout.java
@@ -15,9 +15,11 @@ import javax.swing.WindowConstants;
 public class CLayout {
     JPanel panelContainer = new JPanel();
     MenuPanel menu = new MenuPanel();
-    MenuBotPanel botmenu = new MenuBotPanel();
+    MenuBotPanel botMenu = new MenuBotPanel();
+    GamePanel gamePanel = new GamePanel();
     JButton botsButton = new JButton("Watch The Bots Fight Each Other");
     JButton playerButton = new JButton("Play Vs ...");
+    JButton playGame = new JButton("Play");
     CardLayout cl = new CardLayout();
     Font smallFont = new Font(Font.SANS_SERIF, Font.PLAIN, 20);
 
@@ -27,7 +29,8 @@ public class CLayout {
 
         panelContainer.setLayout(cl);
         panelContainer.add(menu, "1");
-        panelContainer.add(botmenu, "2");
+        panelContainer.add(botMenu, "2");
+        panelContainer.add(gamePanel, "3");
 
         botsButton.setContentAreaFilled(false);
         botsButton.setFont(smallFont);
@@ -37,10 +40,22 @@ public class CLayout {
         playerButton.setFont(smallFont);
         playerButton.setBounds(90, 450, 400, 50);
 
+        playGame.setContentAreaFilled(false);
+        playGame.setFont(smallFont);
+        playGame.setBounds(100, 100, 400, 50);
+
+        menu.add(playGame);
         menu.add(botsButton);
-        botmenu.add(playerButton);
+        botMenu.add(playerButton);
 
         cl.show(panelContainer, "1");
+
+        playGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cl.show(panelContainer, "3");
+            }
+        });
 
         botsButton.addActionListener(new ActionListener() {
             @Override
@@ -58,10 +73,11 @@ public class CLayout {
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setResizable(false);
-        frame.setSize(600, 600);
+        frame.setSize(615, 639);
         frame.setIconImage(image.getImage());
         frame.getContentPane().setBackground(new Color(222, 224, 224));
         frame.add(panelContainer);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
