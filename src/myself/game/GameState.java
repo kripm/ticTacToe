15e0c149487faba727e5b705a95675341317ad3b
kri <93227689+kripm@users.noreply.github.com>
2021-11-13 18:26:24 +0400

    Final.

--- src/myself/game/GameState.java
+++ src/myself/game/GameState.java
@@ -1,21 +1,5 @@
 package myself.game;
 
 public enum GameState {
-    UNFINISHED, DRAW, O_WIN, X_WIN;
-
-    void check(GameState gameState) {
-        switch (gameState) {
-        case X_WIN:
-            System.out.println("X wins");
-            break;
-        case O_WIN:
-            System.out.println("O wins");
-            break;
-        case DRAW:
-            System.out.println("Draw");
-            break;
-        default:
-            break;
-        }
-    }
+    UNFINISHED, DRAW, O_WIN, X_WIN
 }
