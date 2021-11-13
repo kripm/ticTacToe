ce589525335c3c8ded46db8efa727cd41ba27d70
kri <93227689+kripm@users.noreply.github.com>
2021-11-13 11:07:32 +0400

    Player v Player and Player v Bot now works.

--- src/myself/gui/CLayout.java
+++ src/myself/gui/CLayout.java
@@ -1,73 +1,116 @@
 package myself.gui;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import myself.game.Player;
+import myself.game.AI;
+
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.CardLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.WindowConstants;
 
 public class CLayout {
+    Player playerOne = new Player();
+    AI ai;
     JPanel panelContainer = new JPanel();
-    MenuPanel menu = new MenuPanel();
-    MenuBotPanel botMenu = new MenuBotPanel();
-    GamePanel gamePanel = new GamePanel();
-    JButton botsButton = new JButton("Watch The Bots Fight Each Other");
-    JButton playerButton = new JButton("Play Vs ...");
-    JButton playGame = new JButton("Play");
+    JPanel mainMenuPanel = new JPanel();
+    JPanel playButtonPanel = new JPanel();
+    PlayerVsBotMenu playerVsBotMenu = new PlayerVsBotMenu();
+    BotVsBotMenu botVsBotMenu = new BotVsBotMenu();
+    GamePanel playerVsBotGamePanel;
+    GamePanel botVsBotGamePanel;
+    GamePanel playerVsPlayerGamePanel = new GamePanel(new Player(), new Player());
+    JButton playerVsBotButton = new JButton("Player vs Bot");
+    JButton botVsBotButton = new JButton("Bot vs Bot");
+    JButton playerVsPlayerButton = new JButton("Player vs Player");
+    JButton playButtonA = new JButton("Play");
+    JButton playButtonB = new JButton("Play");
+    JButton backButton = new JButton("Back");
     CardLayout cl = new CardLayout();
-    Font smallFont = new Font(Font.SANS_SERIF, Font.PLAIN, 20);
+    Font menuFont = new Font(Font.SANS_SERIF, Font.PLAIN, 30);
 
-    public CLayout() {
+    CLayout() {
         JFrame frame = new JFrame("TicTacToe");
         ImageIcon image = new ImageIcon("src/myself/gui/Images/logo.png");
 
+        mainMenuPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 500, 50));
+        mainMenuPanel.setBounds(0, 0, 600, 600);
         panelContainer.setLayout(cl);
-        panelContainer.add(menu, "1");
-        panelContainer.add(botMenu, "2");
-        panelContainer.add(gamePanel, "3");
+        panelContainer.add(mainMenuPanel, "MainMenu");
+        panelContainer.add(playerVsBotMenu, "PVBM");
+        panelContainer.add(botVsBotMenu, "BVBM");
+        panelContainer.add(playerVsPlayerGamePanel, "PVP");
 
-        botsButton.setContentAreaFilled(false);
-        botsButton.setFont(smallFont);
-        botsButton.setBounds(90, 450, 400, 50);
+        botVsBotButton.setContentAreaFilled(false);
+        botVsBotButton.setFont(menuFont);
 
-        playerButton.setContentAreaFilled(false);
-        playerButton.setFont(smallFont);
-        playerButton.setBounds(90, 450, 400, 50);
+        playerVsBotButton.setContentAreaFilled(false);
+        playerVsBotButton.setFont(menuFont);
 
-        playGame.setContentAreaFilled(false);
-        playGame.setFont(smallFont);
-        playGame.setBounds(100, 100, 400, 50);
+        playerVsPlayerButton.setContentAreaFilled(false);
+        playerVsPlayerButton.setFont(menuFont);
 
-        menu.add(playGame);
-        menu.add(botsButton);
-        botMenu.add(playerButton);
+        playButtonA.setPreferredSize(new Dimension(300, 60));
+        playButtonA.setBackground(Color.LIGHT_GRAY);
+        playButtonA.setFont(menuFont);
+        playButtonPanel.setBorder(BorderFactory.createEmptyBorder(20, 0, 30, 0));
+        playButtonPanel.add(playButtonA);
+
+        mainMenuPanel.add(playerVsBotButton);
+        mainMenuPanel.add(botVsBotButton);
+        mainMenuPanel.add(playerVsPlayerButton);
+        playerVsBotMenu.add(backButton);
+        botVsBotMenu.add(backButton);
 
         cl.show(panelContainer, "1");
 
-        playGame.addActionListener(new ActionListener() {
+        playerVsBotButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                cl.show(panelContainer, "3");
+                playerVsBotMenu.add(playButtonPanel, BorderLayout.SOUTH);
+                cl.show(panelContainer, "PVBM");
+
             }
         });
 
-        botsButton.addActionListener(new ActionListener() {
+        botVsBotButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                cl.show(panelContainer, "2");
+                cl.show(panelContainer, "BVBM");
             }
         });
 
-        playerButton.addActionListener(new ActionListener() {
+        playerVsPlayerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                cl.show(panelContainer, "1");
+                cl.show(panelContainer, "PVP");
+            }
+        });
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cl.show(panelContainer, "MainMenu");
+            }
+        });
+
+        playButtonA.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                playerVsBotGamePanel = new GamePanel(playerOne, playerVsBotMenu.getAI(),
+                        playerVsBotMenu.getPlayerIsX());
+                panelContainer.add(playerVsBotGamePanel, "PVBG");
+                cl.show(panelContainer, "PVBG");
             }
         });
 
@@ -75,7 +118,6 @@ public class CLayout {
         frame.setResizable(false);
         frame.setSize(615, 639);
         frame.setIconImage(image.getImage());
-        frame.getContentPane().setBackground(new Color(222, 224, 224));
         frame.add(panelContainer);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
